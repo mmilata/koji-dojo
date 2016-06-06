8346153eda970b369fa999048d749c2f42dcff16
Vadim Rutkovsky <vrutkovs@redhat.com>
2016-06-06 14:45:21 +0200

    Install latest koji-containerbuild

--- hub/bin/entrypoint.sh
+++ hub/bin/entrypoint.sh
@@ -5,6 +5,8 @@ set -ex
 build-koji.sh
 setup.sh
 
+curl https://raw.githubusercontent.com/release-engineering/koji-containerbuild/master/koji_containerbuild/plugins/hub_containerbuild.py -o /usr/lib/koji-hub-plugins/hub_containerbuild.py
+
 IP=$(find-ip.py)
 
 echo "Starting HTTPd on ${IP}"
