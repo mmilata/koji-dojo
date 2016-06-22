44b7824aaf215426cc40d67462c083934968df69
Vadim Rutkovsky <vrutkovs@redhat.com>
2016-06-22 11:28:58 +0200

    Make sure /usr/lib/koji-hub-plugins exists before downloading latest k-cb builder
    Remove the stale script - its now part of entrypoint.sh

--- hub/bin/entrypoint.sh
+++ hub/bin/entrypoint.sh
@@ -5,6 +5,7 @@ set -ex
 build-koji.sh
 setup.sh
 
+mkdir -p /usr/lib/koji-hub-plugins
 curl https://raw.githubusercontent.com/release-engineering/koji-containerbuild/master/koji_containerbuild/plugins/hub_containerbuild.py -o /usr/lib/koji-hub-plugins/hub_containerbuild.py
 
 IP=$(find-ip.py)
